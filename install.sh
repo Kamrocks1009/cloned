078ac1b42c025670e58ed20d851ff8c3a7983ffc
Kamrocks1009 <kamryn1009@gmail.com>
2016-10-30 00:45:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@ echo "cloning crouton clipboard!"
 git clone http://github.com/zwhitchcox/crouton-clipboard ~/.crouton-clipboard
 echo "adding startup script to .bashrc"
 echo "node ~/.crouton-clipboard/server" >> $HOME/.bashrc
-echo "adding copy/paste commands to vimrc"
+echo "vim
+nnoremap "*p :r !cat $HOME/.crouton-clipboard/data<CR>
+vnoremap "*y :'<,'>w! $HOME/.crouton-clipboard/data<CR>"
 echo "nnoremap \"*p :r !cat $HOME/.crouton-clipboard/data.txt<CR>\nvnoremap \"*y :'<,'>w! $HOME/.crouton-clipboard/data.txt<CR>" >> ~/.vimrc
 echo "starting crouton clipboard!"
 nohup node ~/.crouton-clipboard/server &
